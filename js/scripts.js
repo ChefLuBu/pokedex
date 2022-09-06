3868811d4d1500edeeb9984ea03e0fdc974945d2
Brian Smawley <briansmawley@gmail.com>
2022-09-06 09:05:06 -0400

    Height loop
    Created a loop that iterates over the height value in the pokemonList array

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,21 @@
 let pokemonList = [
     {name: 'Charmander', height:7, type: ['fire']},
     {name:'Bulbasaur', height: 4, type: ['poison', 'grass']},
-    {name:'Squirtle', height: 4, type: ['water']}
-]
+    {name:'Squirtle', height: 2, type: ['water']}
+];
+
+for (let i=0; i < pokemonList.height; i++){
+    if (pokemonList[i].height <7 && pokemonList[i].height >2){
+        console.log(pokemonList[i].name + " is a medium sized pokemon");
+    }else if (pokemonList[i].height < 4){
+        console.log(pokemonList.name + " is a small pokemon");
+    }else if (pokemonList[i].height > 4){
+        console.log(pokemonList[i].name + " is a really big pokemon!")
+    }
+}
+
+
+//create a for loop that iterates over each item in pokemonList:
+// Use document.write() inside the loop’s code to write the Pokémon name on your website’s DOM.
+// Use what you’ve learned about adding strings in JavaScript to write the Pokémon’s height next
+// to its name, for example, “Bulbasaur (height: 7)”.
