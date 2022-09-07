842c6b8eca44aaff9f38d65d5c82f5a5f97a40a2
Brian Smawley <briansmawley@gmail.com>
2022-09-07 10:12:47 -0400

    Fixed the document.write
    Document.write now will only display for Charmander

--- js/scripts.js
+++ js/scripts.js
@@ -14,22 +14,42 @@ let pokemonList = [
     }
 ];
 
-//array of pokemon with names, heights types
 
+
+
+//array of pokemon with names, heights types
+//functions call actions without repasting code over and over again
+//Parameters go inside of functions parentheses when declared
+//Arguments go inside of functions parentheses when called
  
 for (let i=0; i < pokemonList.length; i++){
     if (pokemonList[i].height < 7 && pokemonList[i].height > 2){
-        document.write("<li>" + pokemonList[i].name + " is a medium sized pokemon" + "</li>");
+        console.log(pokemonList[i].name + " is a medium sized pokemon");
     }else if (pokemonList[i].height < 4){
-        document.write("<li>" + pokemonList[i].name + " is a small pokemon") + "</li>";
+        console.log(pokemonList[i].name + " is a small pokemon");
     }else if (pokemonList[i].height > 4){
-        document.write("<li>" + pokemonList[i].name + " is a really big pokemon!" + "</li>")
+        console.log(pokemonList[i].name + " is a really big pokemon!");
+    }if (pokemonList[i].height > 6){
+        document.write("<li>" + pokemonList[i].name + "- Wow, that's big!" + "</li>") 
     }
+
 }
 
 // For loop that specifies pokemon size based on parameters
 
     
+function division(dividend, divisor){
+    if(divisor === 0){
+        return "You're trying to divide by zero."
+    }else{
+        let result = dividend / divisor;
+        return result;
+    }
+}
 
+console.log(division(10, 8));
+console.log(division(2, 11));
+console.log(division(14, 0));
+console.log(division(110, 8));
 
 
