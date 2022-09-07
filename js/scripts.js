cfd1ccd3bb3c9591b5edaba5a731ed33f7d809cd
Brian Smawley <briansmawley@gmail.com>
2022-09-07 15:33:05 -0400

    Updated Script.js with IIFE
    IIFE returns objects with getALL & add. pokemonList now under pokemonRepository IFFE.  forEach() loop updated to work with repository.

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,44 @@
-let pokemonList = [
-    {
-        name: 'Charmander',
-        height:7,
-        type: ['fire']},
-    {
-        name:'Bulbasaur',
-        height: 4,
-        type: ['poison', 'grass']},
-    {
-        name:'Squirtle',
-        height: 2,
-        type: ['water']
+// create a new variable pokemonRepository and assign the IIFE to it
+// The IIFE should return an object with the following public functions assigned as keys: 
+// getAll: return all items (pokemonRepository.getAll(); should return the pokemonList array)
+// add: add a single item to the pokemonList array (calling pokemonRepository.add(item); 
+// should add the Pokémon referred to with item to the pokemonList array)   
+
+let pokemonRepository = (function () {
+    let pokemonList = [
+        {
+            name: 'Charmander',
+            height:7,
+            type: ['Fire']},
+        {
+            name:'Bulbasaur',
+            height: 4,
+            type: ['Poison', 'Grass']},
+        {
+            name:'Squirtle',
+            height: 2,
+            type: ['Water']
+        }
+    ]
+
+    function getAll () {
+        return pokemonList;
     }
-];
+    function add (pokemon) {
+        pokemonList.push(pokemon);
+    }
 
+    
+    return {
+        getAll: getAll,
+        add: add
+    };
 
+})();
+
+console.log(pokemonRepository.getAll());
+pokemonRepository.add({ name: 'Totodile', height: 4, type: 'Water' });
+//add function adds Totodile to the array along with parameters specified
 
 
 //array of pokemon with names, heights types
@@ -22,34 +46,22 @@ let pokemonList = [
 //Parameters go inside of functions parentheses when declared
 //Arguments go inside of functions parentheses when called
  
-for (let i=0; i < pokemonList.length; i++){
-    if (pokemonList[i].height < 7 && pokemonList[i].height > 2){
-        console.log(pokemonList[i].name + " is a medium sized pokemon");
-    }else if (pokemonList[i].height < 4){
-        console.log(pokemonList[i].name + " is a small pokemon");
-    }else if (pokemonList[i].height > 4){
-        console.log(pokemonList[i].name + " is a really big pokemon!");
-    }if (pokemonList[i].height > 6){
-        document.write("<li>" + pokemonList[i].name + "- Wow, that's big!" + "</li>") 
-    }
+let pokemonList = pokemonRepository.getAll();
 
-}
+pokemonList.forEach(function(pokemon){
+    console.log(pokemon.name + ' height: ' + pokemon.height + '' + pokemon.type);
+    document.write('<p>' + pokemon.name + ' height: ' + pokemon.height + ' ' + 'Type:' + ' '  + pokemon.type + '</p>'); 
+})
+
+
+// for (let i = 0; i < pokemonList.length; i++ ) { to loop through each pokemon character
+//   if  (pokemonList[i].height > 1.5) {
+//     document.write('<p>' + pokemonList[i].name + ' height: '  + pokemonList[i].height + ' -Wow, that\'s big!' + '</p>');
+//   } else {
+//       document.write('<p>' + pokemonList[i].name + ' height: '  + pokemonList[i].height + '</p>');  the <br> tag added a line break while the <p> tag also adds a line break and puts each pokemon in a different paragraph
+//   }
+// }
+
 
 // For loop that specifies pokemon size based on parameters
-
-    
-function division(dividend, divisor){
-    if(divisor === 0){
-        return "You're trying to divide by zero."
-    }else{
-        let result = dividend / divisor;
-        return result;
-    }
-}
-
-console.log(division(10, 8));
-console.log(division(2, 11));
-console.log(division(14, 0));
-console.log(division(110, 8));
-
 
