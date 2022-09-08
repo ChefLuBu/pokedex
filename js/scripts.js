c102a5feeca0d32a97d55cf79181828e63eae489
Brian Smawley <briansmawley@gmail.com>
2022-09-07 21:30:10 -0400

    Updated script with Bonus Task
    created filter function
    validated object.keys()
    added typeof parameter
    general code cleanup

--- js/scripts.js
+++ js/scripts.js
@@ -8,36 +8,51 @@ let pokemonRepository = (function () {
     let pokemonList = [
         {
             name: 'Charmander',
-            height:7,
-            type: ['Fire']},
+            height: 7,
+            type: ['Fire']
+        },
         {
-            name:'Bulbasaur',
+            name: 'Bulbasaur',
             height: 4,
-            type: ['Poison', 'Grass']},
+            type: ['Poison', 'Grass']
+        },
         {
-            name:'Squirtle',
+            name: 'Squirtle',
             height: 2,
             type: ['Water']
         }
     ]
 
-    function getAll () {
+    //getAll and add are semantic and its just good practice to use descriptors that make sense
+
+    function getAll() {
         return pokemonList;
     }
-    function add (pokemon) {
-        pokemonList.push(pokemon);
+    function add(pokemon) {
+        let keys = Object.keys(pokemon);
+        if (keys.length === 3 && keys.includes('name') && keys.includes('height') && keys.includes('type')) {
+            if (typeof (pokemon.name) === 'string' && typeof (pokemon.height) === 'number' && Array.isArray(pokemon.type)) {
+                pokemonList.push(pokemon);
+            }
+        }
+    }
+    function getPokemonByName(name) {
+        if (typeof (name) === 'string') {
+            return pokemonList.filter(pokemon => pokemon.name.toLowerCase() === name.toLowerCase());
+        }
     }
 
-    
+
     return {
         getAll: getAll,
-        add: add
+        add: add,
+        getPokemonByName: getPokemonByName
     };
 
 })();
 
 console.log(pokemonRepository.getAll());
-pokemonRepository.add({ name: 'Totodile', height: 4, type: 'Water' });
+pokemonRepository.add({ name: 'Totodile', height: 4, type: ['Water'] });
 //add function adds Totodile to the array along with parameters specified
 
 
@@ -45,15 +60,17 @@ pokemonRepository.add({ name: 'Totodile', height: 4, type: 'Water' });
 //functions call actions without repasting code over and over again
 //Parameters go inside of functions parentheses when declared
 //Arguments go inside of functions parentheses when called
- 
+
 let pokemonList = pokemonRepository.getAll();
 
-pokemonList.forEach(function(pokemon){
+pokemonList.forEach(function (pokemon) {
     console.log(pokemon.name + ' height: ' + pokemon.height + '' + pokemon.type);
-    document.write('<p>' + pokemon.name + ' height: ' + pokemon.height + ' ' + 'Type:' + ' '  + pokemon.type + '</p>'); 
-})
+    document.write('<p>' + pokemon.name + ' height: ' + pokemon.height + ' ' + 'Type:' + ' ' + pokemon.type + '</p>');
+});
 
-
+document.write('<p> Filtered Pokemon List by Name: Squirtle </p>');
+let foundPokemon = pokemonRepository.getPokemonByName('squirtle')[0];
+document.write('<p>' + foundPokemon.name + ' height: ' + foundPokemon.height + ' ' + 'Type:' + ' ' + foundPokemon.type + '</p>');
 // for (let i = 0; i < pokemonList.length; i++ ) { to loop through each pokemon character
 //   if  (pokemonList[i].height > 1.5) {
 //     document.write('<p>' + pokemonList[i].name + ' height: '  + pokemonList[i].height + ' -Wow, that\'s big!' + '</p>');
